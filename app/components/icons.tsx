8601e14fef83123273288c8c7515097c02feef54
ChengYe <666@orgleaf.com>
2023-10-27 10:35:32 +0800

    自定义icon组件增加css

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -1,28 +1,32 @@
 "use client"
-import React from 'react'
+import React, { CSSProperties } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { IconDefinition, faFilm, faTv, faRss, faExternalLink, faCalendarDays, faPenToSquare, faPhotoFilm, faFolderTree, faLink, faHistory, faDatabase, faBookBookmark, faArrowsRotate, faEllipsis, faCloudArrowDown, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import { IconDefinition, faFilm, faTv, faRss, faExternalLink, faCalendarDays, faPenToSquare, faPhotoFilm, faFolderTree, faLink, faHistory, faDatabase, faBookBookmark, faArrowsRotate, faEllipsis, faCloudArrowDown, faSpinner, faEdit } from '@fortawesome/free-solid-svg-icons'
 import Icon from '@ant-design/icons'
 
-const FromFontAwesome = (faIcon: IconDefinition, options?: { fade?: boolean }) => <Icon
-    component={() => <FontAwesomeIcon
-        style={{ width: "1em", height: "1em" }}
-        {...options}
-        icon={faIcon} />} />
+const FromFontAwesome = (faIcon: IconDefinition, options?: { fade?: boolean, }) => {
+    return ({ style }: { style?: CSSProperties }) => (<Icon
+        component={
+            () => <FontAwesomeIcon icon={faIcon} style={{ width: "1em", height: "1em", ...style }} />
+        }
+    />)
+}
 
-export const IconFilmSolid = () => FromFontAwesome(faFilm)
-export const IconTvSolid = () => FromFontAwesome(faTv)
-export const IconRssSolid = () => FromFontAwesome(faRss)
-export const IconCalendarDaysSolid = () => FromFontAwesome(faCalendarDays)
-export const IconCustomSolid = () => FromFontAwesome(faPenToSquare)
-export const IconMediaSolid = () => FromFontAwesome(faPhotoFilm)
-export const IconFolderTreeSolid = () => FromFontAwesome(faFolderTree)
-export const IconLink = () => FromFontAwesome(faLink)
-export const IconHistory = () => FromFontAwesome(faHistory)
-export const IconDatabase = () => FromFontAwesome(faDatabase)
-export const IconExternalLink = () => FromFontAwesome(faExternalLink)
-export const IconBookBookMark = () => FromFontAwesome(faBookBookmark)
-export const IconRefresh = () => FromFontAwesome(faArrowsRotate)
-export const IconEllipsisLoading = () => FromFontAwesome(faEllipsis, { fade: true })
-export const IconDownloader = () => FromFontAwesome(faCloudArrowDown)
-export const IconLoading = () => FromFontAwesome(faSpinner)
+
+export const IconFilmSolid = FromFontAwesome(faFilm)
+export const IconTvSolid = FromFontAwesome(faTv)
+export const IconRssSolid = FromFontAwesome(faRss)
+export const IconCalendarDaysSolid = FromFontAwesome(faCalendarDays)
+export const IconCustomSolid = FromFontAwesome(faPenToSquare)
+export const IconMediaSolid = FromFontAwesome(faPhotoFilm)
+export const IconFolderTreeSolid = FromFontAwesome(faFolderTree)
+export const IconLink = FromFontAwesome(faLink)
+export const IconHistory = FromFontAwesome(faHistory)
+export const IconDatabase = FromFontAwesome(faDatabase)
+export const IconExternalLink = FromFontAwesome(faExternalLink)
+export const IconBookBookMark = FromFontAwesome(faBookBookmark)
+export const IconRefresh = FromFontAwesome(faArrowsRotate)
+export const IconEllipsisLoading = FromFontAwesome(faEllipsis, { fade: true })
+export const IconDownloader = FromFontAwesome(faCloudArrowDown)
+export const IconLoading = FromFontAwesome(faSpinner)
+export const IconEdit = FromFontAwesome(faEdit)
