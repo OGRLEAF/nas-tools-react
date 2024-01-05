53f08ed92b93adb755e5ece35cab131694851d3e
ChengYe <666@orgleaf.com>
2024-01-05 18:52:45 +0800

    localstorage

--- app/utils/storage.ts
+++ app/utils/storage.ts
@@ -1,23 +1,26 @@
+"use client"
 export default class ClientStorage<T> {
     private prefix: string;
-    constructor(name:string) {
+    constructor(name: string) {
         this.prefix = name;
     }
 
-    public setItem(key:string, data:T) {
+    public setItem(key: string, data: T) {
         const storageKey = `${this.prefix}/${key}`
         const dataJson = JSON.stringify(data);
-        return localStorage.setItem(storageKey, dataJson)
+        return window.localStorage.setItem(storageKey, dataJson)
     }
 
-    public getItem(key:string):T|undefined {
+    public getItem(key: string): T | undefined {
         const storageKey = `${this.prefix}/${key}`
-        const dataJson = localStorage.getItem(storageKey)
-        if(dataJson == null) {
-            return undefined;
-        } else {
-            const data = JSON.parse(dataJson) as T;
-            return data;
+        if (typeof window !== 'undefined') {
+            const dataJson = window.localStorage.getItem(storageKey)
+            if (dataJson == null) {
+                return undefined;
+            } else {
+                const data = JSON.parse(dataJson) as T;
+                return data;
+            }
         }
     }
 }
