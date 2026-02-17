e117243f8dadf1201173609bf955fedd4726fb20
orgleaf <666@orgleaf.com>
2026-02-17 04:20:56 +0000

    Refactor ChatMessage

--- app/components/MessageCenter/MessageCenter.tsx
+++ app/components/MessageCenter/MessageCenter.tsx
@@ -1,37 +1,74 @@
 import { API } from "@/app/utils/api/api";
 // import { Message, NastoolMessage } from "@/app/utils/api/message";
-import { Avatar, Button, Card, ConfigProvider, Divider, Flex, Form, Input, List, Space, Switch } from "antd";
-import { UserOutlined } from "@ant-design/icons"
+import { Avatar, Button, Card, Collapse, ConfigProvider, Divider, Flex, Form, Input, List, Space, Spin, Switch } from "antd";
+import { LoadingOutlined, UserOutlined } from "@ant-design/icons"
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import Markdown from "marked-react";
 import { useForm } from "antd/es/form/Form";
-import { ServerMessage, Message } from "@/app/utils/api/message/ServerMessage";
+import { ServerMessage, Message, ChatMessage } from "@/app/utils/api/message/ServerMessage";
 import { ServerConfig } from "@/app/utils/api/serverConfig";
 import { useAPIContext, useEventDataPatch } from "@/app/utils/api/api_base";
-import { ServerEventMsg, useServerEvent, useServerMessage, useSocketio } from "@/app/utils/api/message/ServerEvent";
+import { ServerEventMsg, useServerEvent, useServerMessage, useServerStreamMessage, useSocketio } from "@/app/utils/api/message/ServerEvent";
 
 import "./override.css";
 
-const MessageCard = React.memo(({ msg, isLasted }: { msg: Message, isLasted: boolean, }) => {
-    const messagesEndRef = useRef<HTMLDivElement>(null);
-    return <Card
-        ref={messagesEndRef}
-        variant="borderless" styles={{ body: { paddingBottom: 0, } }}
-    >
-        <Card.Meta
-            style={{ width: "100%", overflowX: "auto" }}
-            avatar={<Avatar icon={<UserOutlined />} />}
-            title={<>{msg.title || "系统"}</>}
-            description={
-                <Markdown value={msg.content.replaceAll("<br>", "\n")} />
-            }
-        />
-        <Divider style={{ margin: "0" }} />
 
-    </Card>
+type MessageIndex = [number, number]
+
+
+
+const MessageCard = React.memo(({ msg, isLasted }: { msg: ChatMessage, isLasted: boolean, }) => {
+    const messagesEndRef = useRef<HTMLDivElement>(null);
+    return <div>
+        <Space orientation="vertical" styles={{ root: { marginTop: "20px" } }}>
+            <Space orientation="horizontal"><Avatar icon={<UserOutlined />} /> {msg.title}</Space>
+            <div style={{ marginLeft: "32px" }}>{
+                msg.title == "LLM" ? <MessageLLMContent msg={msg} /> :
+                    <MessagePlainContent msg={msg} />
+            }</div>
+        </Space>
+    </div>
 })
 
-function MessageList({ msgs }: { msgs: Message[] }) {
+const MessagePlainContent = React.memo(({ msg }: { msg: ChatMessage }) => {
+    if (msg.typ === "text") {
+        return <Markdown value={msg.content} />
+    } else {
+        return <div>
+            {msg.content.map((item, index) => <Markdown key={index} value={item.slice(1).join("")} />)}
+        </div>
+    }
+})
+
+function LLMThinkingSection({ content, loading }: { content: string[], loading?: boolean }) {
+    return <Collapse
+        collapsible="header"
+        defaultActiveKey={[]}
+        items={[
+            {
+                key: '1',
+                label: <Space><span>思考中...</span><Spin indicator={<LoadingOutlined spin />} spinning={loading} size="small" /></Space>,
+                children: content.join("")
+            },
+        ]}
+    />
+}
+const MessageLLMContent = React.memo(({ msg }: { msg: ChatMessage }) => {
+    return <div>
+        {(msg.typ === "layer") ? (
+            msg.content.map((item, index) => {
+                if (item[0] == "thinking") {
+                    return <LLMThinkingSection key={index} content={item.slice(1)} loading={msg.content.length == 1} />
+                } else {
+                    return <Markdown key={index} value={item.slice(1).join("")} />
+                }
+            })
+        ) : <Markdown value={msg.content as string} />}
+    </div>
+})
+
+
+function MessageList({ msgs }: { msgs: ChatMessage[] }) {
     const lastAnchorRef = useRef<HTMLDivElement>(null);
     const lastMsg = msgs[msgs.length - 1];
 
@@ -42,11 +79,12 @@ function MessageList({ msgs }: { msgs: Message[] }) {
             // lastAnchorRef.current.scrollTop = 0;
         }
     }, [lastMsg]);
-    return <div className="message-chat-history" >
 
-        {msgs.map((msg, index) => (<div key={`${index}-${msg.timestamp}`}>
-            <MessageCard msg={msg} isLasted={index == msgs.length - 1} />
-        </div>))
+    return <div className="message-chat-history" >
+        {
+            msgs.map?.((msg, index) => (<div key={`${index}-${msg.timestamp}`}>
+                <MessageCard msg={msg} isLasted={index == msgs.length - 1} />
+            </div>))
         }
         <div ref={lastAnchorRef} style={{ display: "block", height: "10px", width: "10px" }}></div>
     </div>
@@ -75,10 +113,9 @@ const MessageInputForm = React.memo(() => {
     </Form>
 })
 
-
 export default function MessagePanel() {
 
-    const [list, setList] = useState<Message[]>([]);
+    const [list, setList] = useState<ChatMessage[]>([]);
 
     useEventDataPatch(setList, "message_event", { sync: true });
 
